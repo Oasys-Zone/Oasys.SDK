bfde0a79aaa3a7d3b23cf1d57b8bb71862319993
Zero <24566018+ZeroLP@users.noreply.github.com>
2021-06-18 23:43:20 +1000

    1.0.2.2 - Added support for menu

---
+++ Menu/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oasys.SDK.Menu
+{
+    public class MenuManager
+    {
+        /// <summary>
+        /// Adds the tab to the menu.
+        /// </summary>
+        /// <param name="tab"></param>
+        /// <example>
+        ///    <code>
+        ///           MenuManager.AddTab(new Tab("Tab1"));
+        ///    </code>
+        /// </example>
+        public static void AddTab(Common.Menu.Tab tab) => Common.Menu.MenuManagerProvider.AddTab(tab);
+
+        /// <summary>
+        /// Gets the tab with the name from the menu.
+        /// </summary>
+        /// <param name="tabName"></param>
+        /// <returns></returns>
+        /// <example>
+        ///    <code>
+        ///           MenuManager.GetTab("Tab1");
+        ///    </code>
+        /// </example>
+        public static Common.Menu.Tab GetTab(string tabName) => Common.Menu.MenuManagerProvider.GetTab(tabName);
+    }
+}
